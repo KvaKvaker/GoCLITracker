526d988de710814036a2dee41e6f9e38811e2271
Danila Laptop <chuma_kov@mail.ru>
2025-06-04 11:26:44 +0300

    update structure

--- cmd/tracker/main.go
+++ cmd/tracker/main.go
@@ -6,35 +6,9 @@ import (
 	"os"
 )
 
-var helpNote = `-------------------------------------------------------------
-To get help print <file.exe> --help
-To clear all task list <file.exe> clear
-To add task print <file.exe> add "TaskDescription"
-To update task print <file.exe> update <TaskId> "NewTaskDescription"
-To delete task print <file.exe> delete <TaskId>
-To mark task in progress print <file.exe> mark-in-progress <TaskId>
-To mark task done print <file.exe> mark-done <TaskId>
-To mark task todo print <file.exe> mark-todo <TaskId>
-To get tasks list print <file.exe> list <optional mod>
--With list command you can use done, todo, in-progress
--------------------------------------------------------------
-`
-
-type myStruct struct {
-	Tasks []Task
-}
-
-type Task struct {
-	ID          int    `json:"id"`
-	Description string `json:"description"`
-	Status      string `json:"status"`
-	CreatedAt   string `json:"createdAt"`
-	UpdateAt    string `json:"updateAt"`
-}
-
 func main() {
 	if len(os.Args) < 2 || os.Args[1] == "--help" {
-		fmt.Println(helpNote)
+		fmt.Println(task.HelpNote)
 	} else {
 		var jsonFile *os.File
 		switch os.Args[1] {
--- task/model.go
+++ task/model.go
@@ -6,6 +6,20 @@ const (
 	StatusDone       = "Done"
 )
 
+var HelpNote = `-------------------------------------------------------------
+To get help print <file.exe> --help
+To clear all task list <file.exe> clear
+To add task print <file.exe> add "TaskDescription"
+To update task print <file.exe> update <TaskId> "NewTaskDescription"
+To delete task print <file.exe> delete <TaskId>
+To mark task in progress print <file.exe> mark-in-progress <TaskId>
+To mark task done print <file.exe> mark-done <TaskId>
+To mark task todo print <file.exe> mark-todo <TaskId>
+To get tasks list print <file.exe> list <optional mod>
+-With list command you can use done, todo, in-progress
+-------------------------------------------------------------
+`
+
 type TaskList struct {
 	Tasks []Task `json:"tasks"`
 }
